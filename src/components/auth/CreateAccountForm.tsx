81bc521cc5bb56b76a9fc6fe2c87d2517c0b1bb9
gwkim <qkek875@gmail.com>
2023-07-06 15:46:37 +0900

    회원가입 페이지 수정

--- src/components/auth/CreateAccountForm.tsx
+++ src/components/auth/CreateAccountForm.tsx
@@ -18,7 +18,7 @@ const CreateAccountForm = () => {
         const enteredPassword = passwordInputRef.current!.value;
         const enteredNickname = nicknameInputRef.current!.value;
         const enteredEmail = emailInputRef.current!.value;
-        authCtx.signUp(enteredUserId, enteredPassword,enteredEmail, enteredNickname);
+        authCtx.signUp(enteredUserId, enteredPassword, enteredEmail, enteredNickname);
 
         if (authCtx.isSuccess) {
             return navigate("/", {replace: true});
@@ -29,30 +29,62 @@ const CreateAccountForm = () => {
     }
 
     return (
-        <section >
-            <h1>Create Account</h1>
-            <form onSubmit={submitHandler}>
-                <div >
-                    <label htmlFor='userid'>Your id</label>
-                    <input type='id' id='userid' required ref={userIdInputRef}/>
+        <div className="container">
+            <div className="input-form-background row">
+                <div className="input-form col-md-12 mx-auto">
+                    <h4 className="mb-3 mt-4">회원가입</h4>
+                    <form onSubmit={submitHandler}>
+                        <div className="row">
+                            <div className="col-md-6 mb-3">
+                                <label htmlFor="name">이름</label>
+                                <input type="text" className="form-control" id="name" placeholder="" required/>
+                                <div className="invalid-feedback">
+                                    이름을 입력해주세요.
+                                </div>
+                            </div>
+                            <div className="col-md-6 mb-3">
+                                <label htmlFor="nickname">별명</label>
+                                <input type="text" className="form-control" id="nickname" placeholder=""
+                                       required/>
+                                <div className="invalid-feedback">
+                                    별명을 입력해주세요.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="email">이메일</label>
+                            <input type="email" className="form-control" id="email" placeholder="you@example.com"
+                                   required/>
+                            <div className="invalid-feedback">
+                                이메일을 입력해주세요.
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="address">주소</label>
+                            <input type="text" className="form-control" id="address" placeholder="서울특별시 강남구" required/>
+                            <div className="invalid-feedback">
+                                주소를 입력해주세요.
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="address2">상세주소<span className="text-muted">&nbsp;(필수 아님)</span></label>
+                            <input type="text" className="form-control" id="address2" placeholder="상세주소를 입력해주세요."/>
+                        </div>
+
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="aggrement" required/>
+                            <label className="custom-control-label" htmlFor="aggrement">개인정보 수집 및 이용에 동의합니다.</label>
+                        </div>
+                        <div className="mb-4"></div>
+                        <button className="btn btn-primary btn-lg btn-block" type="submit">가입 완료</button>
+                    </form>
                 </div>
-                <div >
-                    <label htmlFor='email'>Your email</label>
-                    <input type='email' id='email' required ref={emailInputRef}/>
-                </div>
-                <div >
-                    <label htmlFor="password">Your password</label>
-                    <input type='password' id='password' required ref={passwordInputRef}/>
-                </div>
-                <div >
-                    <label htmlFor="nickname">NickName</label>
-                    <input type='text' id='nickname' required ref={nicknameInputRef}/>
-                </div>
-                <div >
-                    <button type='submit'>Submit</button>
-                </div>
-            </form>
-        </section>
+            </div>
+        </div>
+
     );
 };
 
